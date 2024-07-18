bbb3a759b9cf160dd2e0b0cabdfeae03008d9abc
HerbertUnterberger <59275992+HerbertUnterberger@users.noreply.github.com>
2024-07-18 13:05:34 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
-import { NordpoolPlatform } from './platform';
+import { PLATFORM_NAME } from './settings.js';
+import { NordpoolPlatform } from './platform.js';
 
 /**
  * This method registers the platform with Homebridge
