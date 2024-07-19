29d9efc4959012ead94835a8df58d029b11c3fcd
HerbertUnterberger <59275992+HerbertUnterberger@users.noreply.github.com>
2024-07-19 07:50:14 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -8,7 +8,7 @@ import { Cache } from 'file-system-cache';
 const pkg = require('../package.json');
 
 export const PLATFORM_NAME = 'Platform_aWATTar';
-export const PLUGIN_NAME = 'homebridge-awattar';
+export const PLUGIN_NAME = 'plugin_awattar';
 export const PLATFORM_MANUFACTURER = pkg.author.name;
 export const PLATFORM_VERSION = pkg.version;
 export const PLATFORM_MODEL = 'Electricity price sensors';
