3fff1482923a130ced31fe63b191a863e723d666
HerbertUnterberger <59275992+HerbertUnterberger@users.noreply.github.com>
2025-07-29 14:36:13 +0200

    deleted

--- src/settings.ts
+++ src/settings.ts
@@ -107,28 +107,14 @@ export function defaultPricesCache(api: API) {
   const cacheDirectory = Path.join(storagePath, '.cache');
 
   // check if directory .cache exists
-  //fs.access(cacheDirectory, (error) => {
-  if (!fs.existsSync(cacheDirectory)) {
-      fs.mkdirSync(cacheDirectory, { recursive: true });
-      log('New Directory created successfully !!');
-   } else {
-      log('Given Directory already exists !!');
-    }
-    // To check if the given directory already exists or not
-    //if (error) {
-      // If current directory does not exist then create it
-    //  fs.mkdir(cacheDirectory, (error) => {
-    //    if (error) {
-    //      log(error);
-    //    } else {
-    //      log('New Directory created successfully !!');
-    //    }
-    //  });
-    //} else {
-    //  log('Given Directory already exists !!');
-    //}
   
-
+  if (!fs.existsSync(cacheDirectory)) {
+    fs.mkdirSync(cacheDirectory, { recursive: true });
+    log('New Directory created successfully !!');
+   } else {
+    log('Given Directory already exists !!');
+    }
+  
   // auto-cleanup of old cached files on init
   const files = fs.readdirSync(cacheDirectory);
   const now = Date.now();
